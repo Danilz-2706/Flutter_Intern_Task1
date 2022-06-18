0b69f134cced26f8e9da4ec06b14884f68013a9c
Dark Traceur <tandat27062000@gmail.com>
2022-06-18 07:57:21 +0700

    fix update file key.dart

---
+++ lib/key.dart
@@ -0,0 +1,9 @@
+const AUTH0_DOMAIN = 'dev-72zjj907.us.auth0.com';
+// ignore: constant_identifier_names
+const AUTH0_CLIENT_ID = 'J6mQaAOX4ZVjajBVJgEuXBGIenav8vpm';
+
+// ignore: constant_identifier_names
+const AUTH0_REDIRECT_URI = 'com.example.finalcry://login-callback';
+// const AUTH0_REDIRECT_URI = 'https://$AUTH0_DOMAIN/login/callback';
+// ignore: constant_identifier_names
+const AUTH0_ISSUER = 'https://$AUTH0_DOMAIN';
